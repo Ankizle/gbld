60c68db48c5258291c3708dcf6433029a0c91c7c
Ankizle <ankizleyt@gmail.com>
2025-06-02 02:55:22 -0400

    added a check in MaxTimestamp if there are no files to check

--- fs/max_timestamp.go
+++ fs/max_timestamp.go
@@ -1,10 +1,17 @@
 package gbld_fs
 
 import (
+	"math"
+
 	"github.com/Ankizle/gbld"
 )
 
 func MaxTimestamp(fs []gbld.File) uint64 {
+
+	if len(fs) == 0 {
+		return math.MaxUint64
+	}
+
 	var max uint64 = 0
 	for _, f := range fs {
 		t := Timestamp(f)
