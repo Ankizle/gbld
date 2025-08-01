75e6ca11ab435554b6a59ba6da77a2f84bb26904
ankizle <ankizleyt@gmail.com>
2025-08-01 13:28:09 -0400

    filed change_ext to work with multiple extensions (e.g. .so.3)

--- fs/change_ext.go
+++ fs/change_ext.go
@@ -1,12 +1,10 @@
 package gbld_fs
 
 import (
-	"path/filepath"
 	"strings"
 )
 
 func ChangeExt(name string, new_ext string) string {
-	ext := filepath.Ext(name)
-	new_name := strings.TrimSuffix(name, ext)
-	return new_name + new_ext
+	base_noext := strings.Split(name, ".")[0]
+	return base_noext + new_ext
 }
