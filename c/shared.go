3f7867b7f2b3ec5d90e1780c6ca87d4069f9acf2
ankizle <ankizleyt@gmail.com>
2025-08-01 13:21:00 -0400

    added shared library versions

--- c/shared.go
+++ c/shared.go
@@ -1,6 +1,7 @@
 package gbld_c
 
 import (
+	"fmt"
 	"path/filepath"
 	"strings"
 
@@ -8,11 +9,7 @@ import (
 	gbld_fs "github.com/Ankizle/gbld/fs"
 )
 
-func Shared(os string, f string) gbld.File {
-	src_path := f
-	var prefix string
-	var ext string
-
+func GetSharedPreExt(os string) (prefix string, ext string) {
 	switch os {
 	case "linux":
 		prefix = "lib"
@@ -21,9 +18,28 @@ func Shared(os string, f string) gbld.File {
 		ext = ".dll"
 	}
 
-	shared_path := gbld_fs.ChangeExt(src_path, ext)
+	return prefix, ext
+}
+
+func Shared(os string, f string) gbld.File {
+	prefix, ext := GetSharedPreExt(os)
+
+	shared_path := gbld_fs.ChangeExt(f, ext)
 	shared_path = gbld_fs.AddPrefix(shared_path, prefix)
 	return gbld.NewFile(shared_path)
+}
+
+func SharedVersion(f gbld.File, major uint, minor uint) gbld.File {
+	ext := filepath.Ext(f.Path())
+
+	if major != 0 {
+		ext += fmt.Sprintf(".%d", major)
+	}
+	if minor != 0 {
+		ext += fmt.Sprintf(".%d", minor)
+	}
+
+	return gbld.NewFile(gbld_fs.ChangeExt(f.Path(), ext))
 }
 
 func FromSharedFile(os string, f gbld.File) string {
